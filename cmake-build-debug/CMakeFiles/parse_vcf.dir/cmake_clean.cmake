ec0346dc1365da22e41d7929ec9de083ad64e913
Akado2009 <prosvirov.k@gmail.com>
2019-02-08 14:45:54 -0800

    build added

---
+++ cmake-build-debug/CMakeFiles/parse_vcf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parse_vcf.dir/parse_vcf.cpp.o"
+  "parse_vcf.pdb"
+  "parse_vcf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parse_vcf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ parse_vcf.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+//HEADER
+//#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
+// 1	182382427	293897	C	T	.	.	ALLELEID=278615;CLNDISDB=MedGen:C1864910,OMIM:610015,Orphanet:ORPHA71278;CLNDN=Glutamine_deficiency,_congenital;CLNHGVS=NC_000001.11:g.182382427C>T;CLNREVSTAT=criteria_provided,_single_submitter;CLNSIG=Uncertain_significance;CLNVC=single_nucleotide_variant;CLNVCSO=SO:0001483;CLNVI=Illumina_Clinical_Services_Laboratory,Illumina:479995;GENEINFO=GLUL:2752;MC=SO:0001624|3_prime_UTR_variant;ORIGIN=1;RS=868130362
+
+int main() {
+    int benings = 0;
+    int pathogenic = 0;
+    int overall = 0;
+
+    std::ifstream read("data/clinvar_20190204.vcf");
+    std::ofstream benings_write("data/benings.tab");
+    std::ofstream pathogenic_write("data/pathogenic.tab");
+
+    benings_write << "Chromosome\tPosition\tID\tFrom\tTo" << std::endl;
+    pathogenic_write << "Chromosome\tPosition\tID\tFrom\tTo" << std::endl;
+
+    for(std::string line; std::getline(read, line); ) {
+        if (line.rfind('#', 0) != 0) {
+            std::vector<std::string> result;
+            std::istringstream line_stream(line);
+            std::string substring;
+            while (std::getline(line_stream, substring, '\t'))
+                result.push_back(substring);
+            overall++;
+            if (result[3].size() == 1 && result[4].size() == 1) {
+                if (result[7].find("Pathogenic") != std::string::npos) {
+                    pathogenic++;
+                    pathogenic_write << result[0] << "\t" << result[1] << "\t" << result[2] << "\t" << result[3] << "\t"
+                                     << result[4] << std::endl;
+                } else {
+                    benings++;
+                    benings_write << result[0] << "\t" << result[1] << "\t" << result[2] << "\t" << result[3] << "\t"
+                                  << result[4] << std::endl;
+                }
+            }
+        }
+    }
+//    str.find(str2) != string::npos
+    read.close();
+
+    std::ofstream write("vcf_info.txt");
+    write << "Overall number: " << overall << std::endl;
+    write << "Benings number: " << benings << std::endl;
+    write << "Pathogenic number: " << pathogenic << std::endl;
+    write.close();
+    benings_write.close();
+    pathogenic_write.close();
+    return 0;
+}
